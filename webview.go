38a98d51c27e6941ceeebf4c79c6a6d743ac0f95
hunterfaucher <hrfaucher@gmail.com>
2020-08-19 22:02:51 -0500

    started splitting the rtsp thread from image processing

--- webview.go
+++ webview.go
@@ -17,27 +17,30 @@ import (
 var (
 	err     error
 	webcam  *gocv.VideoCapture
-	webcam1 *gocv.VideoCapture
 	frameid int
 )
 
 var buffer = make(map[int][]byte)
 var frame []byte
 var mutex = &sync.Mutex{}
+var mutex2 = &sync.Mutex{}
 var classifierFile string
+var imgcv = gocv.NewMat()
 
 func main() {
 
 	host := "0.0.0.0:3000"
 
 	target1 := flag.String("target1", "", "enter camera id or rtsp ip")
-	target2 := flag.String("target2", "", "enter camera id or rtsp ip")
+
 	xmlFile := flag.String("xml", "", "path to xml file")
 	flag.Parse()
-	if *target1 == "" || *xmlFile == "" || *target2 == "" {
+	if *target1 == "" || *xmlFile == "" {
 		flag.Usage()
 		return
 	}
+
+	defer imgcv.Close()
 
 	classifierFile = *xmlFile
 
@@ -49,16 +52,9 @@ func main() {
 	}
 	defer webcam.Close()
 
-	webcam1, err = gocv.VideoCaptureFile(*target2)
-
-	if err != nil {
-		fmt.Printf("Error opening capture device: \n")
-		return
-	}
-	defer webcam1.Close()
-
 	// start capturing
 	go getframes()
+	go processFrame()
 
 	fmt.Println("Capturing. Open http://" + host)
 
@@ -67,8 +63,8 @@ func main() {
 		w.Header().Set("Content-Type", "multipart/x-mixed-replace; boundary=frame")
 		data := ""
 		for {
-			/*			fmt.Println("Frame ID: ", frame_id)
-			 */mutex.Lock()
+			//fmt.Println("Frame ID: ", frameid)
+			mutex.Lock()
 			data = "--frame\r\n  Content-Type: image/jpeg\r\n\r\n" + string(frame) + "\r\n\r\n"
 			mutex.Unlock()
 			time.Sleep(33 * time.Millisecond)
@@ -84,13 +80,7 @@ func main() {
 	log.Fatal(http.ListenAndServe(host, nil))
 }
 
-func getframes() {
-	img := gocv.NewMat()
-	defer img.Close()
-
-	gimg := gocv.NewMat()
-	defer gimg.Close()
-
+func processFrame() {
 	blue := color.RGBA{0, 0, 255, 0}
 
 	classifier := gocv.NewCascadeClassifier()
@@ -100,7 +90,38 @@ func getframes() {
 		fmt.Printf("Error reading cascade file: %v\n", classifierFile)
 		return
 	}
+	for {
 
+		if imgcv.Empty() {
+			continue
+		}
+
+		mutex2.Lock()
+		rects := classifier.DetectMultiScale(imgcv)
+
+		fmt.Printf("found %d faces\n", len(rects))
+		//i := 0
+		//mutex.Lock()
+
+		for _, r := range rects {
+			//fmt.Printf("%v\n", r)
+			//i++
+			//mutex.Lock()
+			gocv.Rectangle(&imgcv, r, blue, 3)
+			//mutex.UnLock()
+			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			gocv.PutText(&imgcv, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+		}
+		mutex2.Unlock()
+	}
+
+}
+
+func getframes() {
+
+	img := gocv.NewMat()
+	defer img.Close()
 	for {
 		if ok := webcam.Read(&img); !ok {
 			fmt.Printf("Device closed\n")
@@ -109,40 +130,53 @@ func getframes() {
 		if img.Empty() {
 			continue
 		}
+		frameid++
 
-		if ok := webcam1.Read(&gimg); !ok {
-			fmt.Printf("Device closed\n")
-			return
-		}
-		if gimg.Empty() {
-			continue
-		}
+		imgcv = img
 
 		gocv.Resize(img, &img, image.Point{}, float64(0.5), float64(0.5), 0)
-		gocv.Resize(gimg, &gimg, image.Point{}, float64(0.5), float64(0.5), 0)
-
-		rects := classifier.DetectMultiScale(gimg)
-
-		//fmt.Printf("found %d faces\n", len(rects))
-		//i := 0
-		//mutex.Lock()
-		for _, r := range rects {
-			//fmt.Printf("%v\n", r)
-			//i++
-			//mutex.Lock()
-			gocv.Rectangle(&img, r, blue, 3)
-			//mutex.UnLock()
-			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
-			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
-			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
-
-		}
-
-		//time.Sleep(1 * time.Millisecond)
-		//frameid++
-		//fmt.Printf("%v\n", frameid)
-
 		frame, _ = gocv.IMEncode(".jpg", img)
 
 	}
+	/*
+		for {
+			if ok := webcam.Read(&img); !ok {
+				fmt.Printf("Device closed\n")
+				return
+			}
+			if img.Empty() {
+				continue
+			}
+
+			img2 := img
+
+			frame, _ = gocv.IMEncode(".jpg", img)
+
+			gocv.Resize(img2, &img2, image.Point{}, float64(0.5), float64(0.5), 0)
+
+			rects := classifier.DetectMultiScale(img2)
+
+			fmt.Printf("found %d faces\n", len(rects))
+			//i := 0
+			//mutex.Lock()
+
+			for _, r := range rects {
+				//fmt.Printf("%v\n", r)
+				//i++
+				//mutex.Lock()
+				gocv.Rectangle(&img2, r, blue, 3)
+				//mutex.UnLock()
+				size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+				pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+				gocv.PutText(&img2, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+
+			}
+
+			time.Sleep(1 * time.Millisecond)
+			//frameid++
+			//fmt.Printf("%v\n", frameid)
+
+			//frame, _ = gocv.IMEncode(".jpg", img)
+
+		}*/
 }
